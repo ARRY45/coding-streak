1f4c24b6ae2497176b8ba49097da3cc930e15d27
ARRY45 <litexpro92@gmail.com>
2026-08-10 01:41:02 +0530

    DAY 6

---
+++ ch5qq1.py
@@ -0,0 +1,11 @@
+d = {
+    "madad" : "help",
+    "kamm" : "low",
+    "jao" : "go",
+    "pani" : "water",
+    "mummy" : "mom"
+}
+
+word = input("Enter the word you want meaning of :")
+
+print(d[word])
