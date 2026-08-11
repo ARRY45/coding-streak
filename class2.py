64ced989508b7bac49d0b9fe875bf45bf2de2ce6
ARRY45 <litexpro92@gmail.com>
2026-08-12 01:11:55 +0530

    Day 8

---
+++ class2.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+
+print("Addition of first and second number = ",a+b)
+print("Subtraction of first and second number = ",a-b)
+print("Multiplication of first and second number = ",a*b)
+print("Division of first and second number = ",a/b)
+print("Modulas of first and second number = ",a%b)
