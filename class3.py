7598178a7b011b955bc22f58520f7833b7b715ea
ARRY45 <litexpro92@gmail.com>
2026-08-12 01:22:30 +0530

    day 8

---
+++ class3.py
@@ -0,0 +1,14 @@
+length = float(input("Enter length of rectangle :"))
+breadth = float(input("Enter breadth of rectangle :"))
+print("Area of rectangle is :",length*breadth )
+
+
+side = float(input("Enter the side of square :"))
+print("Area of square =",side*side)
+
+base = float(input("Enter the base of triangle :"))
+height = float(input("Enter the height of triangle :"))
+print("Area of triangle =", 1/2*base*height)
+
+radius = float(input("Enter the radius of the circle :"))
+print("Area of circle =", 3.14*radius*radius)
