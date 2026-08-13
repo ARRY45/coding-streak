5394da90515f8cf1de023a70530c4161f2cfce39
ARRY45 <litexpro92@gmail.com>
2026-08-14 01:43:57 +0530

    dday 10

---
+++ class14.py
@@ -0,0 +1,15 @@
+count = 0
+
+for num in range(2, 101):
+    prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            prime = False
+            break
+
+    if prime:
+        print(num)
+        count += 1
+
+print("Total prime numbers =", count)
