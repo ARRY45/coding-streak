4dfec3c8585917caa9e81ac38eeea19055cc036f
ARRY45 <litexpro92@gmail.com>
2026-08-16 03:06:50 +0530

    day 12

---
+++ ch6q6.py
@@ -0,0 +1,19 @@
+marks = int(input("ENTER YOUR MARKS :"))
+
+if (marks<=100 and marks>=90):
+    print("GRADE EX",marks)    
+
+elif (marks<=90 and marks>=80):
+    print("GRADE A",marks)   
+ 
+elif (marks<=80 and marks>=70):
+    print("GRADE B",marks)    
+
+elif (marks<=70 and marks>=60):
+    print("GRADE C",marks)    
+
+elif (marks<=60 and marks>=50):
+    print("GRADE D",marks)    
+
+elif (marks<=50):
+    print("GRADE F")    
