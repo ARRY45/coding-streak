8c0b423ab14483745c6c55aa1d7024e891eae79f
ARRY45 <litexpro92@gmail.com>
2026-08-16 02:54:47 +0530

    day12

---
+++ ch6q5.py
@@ -0,0 +1,10 @@
+list = ["Aryan","Navjot","Saurav","Aastha","Pranav","Ankita","Tarni"]
+
+name = input("Enter your name :")
+
+if (name in list):
+    print("YOUR NAME IS IN THE LIST THANKYOU!")
+
+else:
+    print("SORRY! YOUR NAME IS NOT IN THE LIST")
+
